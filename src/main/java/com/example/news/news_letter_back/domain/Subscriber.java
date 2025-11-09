985a9051aa928ed03272558454c5a88a52ea551c
moonformeli <mgyang95@gmail.com>
2025-11-10 04:57:14 +0900

    feat: subscriber 테이블 추가

---
+++ src/main/java/com/example/news/news_letter_back/domain/Subscriber.java
@@ -0,0 +1,37 @@
+package com.example.news.news_letter_back.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "subscriber")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subscriber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "status_acode", nullable = false)
+    private String statusAcode;
+
+    @Column(name = "status_bcode", nullable = false)
+    private String statusBcode;
+
+    @Column(name = "unsubscribe_token", nullable = false, unique = true)
+    private String unsubscribeToken;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt = Instant.now();
+}
